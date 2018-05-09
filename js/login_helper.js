c939223fe4853b0c96e0abe5983e1643f403dcbc
SaturdayAM <jwl62@georgetown.edu>
2018-05-09 15:39:26 -0400

    login buttons

--- js/login_helper.js
+++ js/login_helper.js
@@ -1,44 +1,54 @@
 // add event listener to login button
 (function () {
+  let globalCurrentUser;
   let loginButton = document.getElementById("login-button");
-  function login (username) {
-    let toReturn;
-    // fetch
-    let apiUrl = "http://localhost:3000/signin";
-    let fullApiUrl = apiUrl + "?username=" + username;
-    fetch(fullApiUrl, { method: "POST" })
-    .then(response => {
-      console.log("in .then #1, initial response:", response);
-      return response.json()
-    })
-    .then(parsedJson => {
-      console.log("in .then #2, parsed json:", parsedJson);
-      console.log("toReturn1:", toReturn);
-      toReturn = parsedJson;
-      console.log("toReturn2:", toReturn);
-    });
-    // should return a promise
-  }; // <-- end of login()
-
+  loginButton.addEventListener("click", event => loginHandler(event));
+  
   function loginHandler (event) {
     // get input field
     let loginInput = document.getElementById("login-input");
     // errors: empty?
     if (loginInput.value.length < 1) {
-      // will need to produce an alert
       alert("Please enter username.")
-    } else {
-      // login
-      let a = login(loginInput.value);
-      debugger;
-      // global 'currentUserId' = id of user
-        // whose username was in input field
-      // add 'welcome <user>' in place of input field
-        // and logout button
+    }else { //login
+        loginFetch(loginInput.value)
+          .then(loginAttempt => {
+            loginAttempt === "Failure"? resetLogin() : loginDOM(loginAttempt);
+          })
+          // global 'currentUserId' = id of user
+            // whose username was in input field
+          // add 'welcome <user>' in place of input field
+            // and logout button
     }
     // errors: valid username?
 
   }; // <-- end of loginHandler()
 
-  loginButton.addEventListener("click", event => loginHandler(event));
+  function resetLogin(){
+    let loginInput = document.getElementById("login-input");
+    loginInput.value = ""; 
+    alert("Incorrect Login!");
+    loginInput.focus();
+  }
+
+  function loginDOM(currentUser){
+      globalCurrentUser = currentUser;
+      let loginButton = document.getElementById("login-button");
+      loginButton.id = "logout-button";
+      loginButton.innerHTML = "LOGOUT";
+      debugger;
+      alert(JSON.stringify(currentUser));
+  }
+
+  
+
+  function loginFetch (username) {
+    let apiUrl = "http://localhost:3000/signin";
+    let fullApiUrl = apiUrl + "?username=" + username;
+    return fetch(fullApiUrl, { method: "POST" })
+      .then(response => {
+        return response.status === 200? response.json() : "Failure";
+      });
+  }; // <-- end of loginFetch()
+
 })()

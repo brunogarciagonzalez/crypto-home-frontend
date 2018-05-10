498e5b8439775436e4eb3c54386bc7149191d651
SaturdayAM <jwl62@georgetown.edu>
2018-05-10 09:39:20 -0400

    getting logout to work

--- js/login_helper.js
+++ js/login_helper.js
@@ -1,6 +1,14 @@
-  let globalCurrentUser;
-  let loginButton = document.getElementById("login-button");
-  loginButton.addEventListener("click", event => loginHandler(event));
+  globalCurrentUser = null;
+
+  function initializeLoginEvent(){
+    let loginButton = document.getElementById("login-button");
+    loginButton.addEventListener("click", event => loginHandler(event));
+  }
+
+  function initializeLoginEvent(){
+    
+  }
+
 
   function loginHandler (event) {
     // get input field
@@ -13,12 +21,22 @@
           .then(loginAttempt => {  // errors: valid username? 
             loginAttempt === "Failure"? resetLogin() : loginDOM(loginAttempt);
             let logoutButton = document.getElementById("logout-button");
-            logoutButton.addEventListener("click", event => console.log("logout"));
+            logoutButton.addEventListener("click", event => logoutHandler(event));
           })
           // global 'currentUserId' = id of user
             // whose username was in input field
     }
   }; // <-- end of loginHandler()
+
+  function logoutHandler(event){
+    globalCurrentUser = null;
+    document.getElementById('welcome-msg').remove();
+    switchButtons();
+    let loginContainer = document.getElementById('login-container');
+    loginContainer.innerHTML = '<input id="login-input" type="text">' +
+                               loginContainer.innerHTML;
+    initializeLoginEvent();
+  }
 
   function resetLogin(){
     let loginInput = document.getElementById("login-input");
@@ -33,11 +51,12 @@
       addNameToDOM(currentUser);
   }
 
+
   function addNameToDOM(currentUser){
     let loginContainer = document.getElementById("login-container");
     let loginInput = document.getElementById("login-input");
     loginInput.remove();
-    loginContainer.innerHTML = `<p style="display: inline-block">Welcome, ${currentUser.username} </p>` + 
+    loginContainer.innerHTML = `<p id = "welcome-msg" style="display: inline-block">Welcome, ${currentUser.username} </p>` + 
                                loginContainer.innerHTML;
   }
   function switchButtons(){
@@ -51,7 +70,6 @@
           loginButton.classList.add("invisible");
       }
   }
-
   function loginFetch (username) {
     let apiUrl = "http://localhost:3000/signin";
     let fullApiUrl = apiUrl + "?username=" + username;
@@ -60,4 +78,12 @@
         return response.status === 200? response.json() : "Failure";
       });
   }; // <-- end of loginFetch()
+
+
+
+
+// ********************** Actions, for now *************************
+initializeLoginEvent();
+
+
 
